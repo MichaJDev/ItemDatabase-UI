0ec2f4ef02f044165e88f11fc930c10c32a36872
MichaJ <michajanssen93@gmail.com>
2018-10-01 19:01:36 +0200

    Initial Commit Working UI
    Done:
    Created Working UI
    - Added Name,Desc,Stats,Type,Worth
    TODO:
    
    Database adding/ItemType ENUMS

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dbBtn = new System.Windows.Forms.Button();
+            this.addItemBtn = new System.Windows.Forms.Button();
+            this.itemStrCheck = new System.Windows.Forms.CheckBox();
+            this.itemIntCheck = new System.Windows.Forms.CheckBox();
+            this.itemAgilCheck = new System.Windows.Forms.CheckBox();
+            this.itemHastCheck = new System.Windows.Forms.CheckBox();
+            this.itemMastCheck = new System.Windows.Forms.CheckBox();
+            this.itemStamCheck = new System.Windows.Forms.CheckBox();
+            this.itemType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.itemWorth = new System.Windows.Forms.ComboBox();
+            this.itemInt = new System.Windows.Forms.TextBox();
+            this.itemStr = new System.Windows.Forms.TextBox();
+            this.itemStam = new System.Windows.Forms.TextBox();
+            this.itemMas = new System.Windows.Forms.TextBox();
+            this.itemHas = new System.Windows.Forms.TextBox();
+            this.itemAgi = new System.Windows.Forms.TextBox();
+            this.itemDesc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.itemName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbBtn
+            // 
+            this.dbBtn.Location = new System.Drawing.Point(18, 302);
+            this.dbBtn.Name = "dbBtn";
+            this.dbBtn.Size = new System.Drawing.Size(75, 23);
+            this.dbBtn.TabIndex = 0;
+            this.dbBtn.Text = "Database";
+            this.dbBtn.UseVisualStyleBackColor = true;
+            // 
+            // addItemBtn
+            // 
+            this.addItemBtn.Location = new System.Drawing.Point(197, 302);
+            this.addItemBtn.Name = "addItemBtn";
+            this.addItemBtn.Size = new System.Drawing.Size(75, 23);
+            this.addItemBtn.TabIndex = 1;
+            this.addItemBtn.Text = "Add Item";
+            this.addItemBtn.UseVisualStyleBackColor = true;
+            this.addItemBtn.Click += new System.EventHandler(this.addItemBtn_Click);
+            // 
+            // itemStrCheck
+            // 
+            this.itemStrCheck.AutoSize = true;
+            this.itemStrCheck.Location = new System.Drawing.Point(157, 192);
+            this.itemStrCheck.Name = "itemStrCheck";
+            this.itemStrCheck.Size = new System.Drawing.Size(66, 17);
+            this.itemStrCheck.TabIndex = 2;
+            this.itemStrCheck.Text = "Strength";
+            this.itemStrCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemIntCheck
+            // 
+            this.itemIntCheck.AutoSize = true;
+            this.itemIntCheck.Location = new System.Drawing.Point(157, 212);
+            this.itemIntCheck.Name = "itemIntCheck";
+            this.itemIntCheck.Size = new System.Drawing.Size(63, 17);
+            this.itemIntCheck.TabIndex = 3;
+            this.itemIntCheck.Text = "Intellect";
+            this.itemIntCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemAgilCheck
+            // 
+            this.itemAgilCheck.AutoSize = true;
+            this.itemAgilCheck.Location = new System.Drawing.Point(157, 235);
+            this.itemAgilCheck.Name = "itemAgilCheck";
+            this.itemAgilCheck.Size = new System.Drawing.Size(53, 17);
+            this.itemAgilCheck.TabIndex = 4;
+            this.itemAgilCheck.Text = "Agility";
+            this.itemAgilCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemHastCheck
+            // 
+            this.itemHastCheck.AutoSize = true;
+            this.itemHastCheck.Location = new System.Drawing.Point(157, 257);
+            this.itemHastCheck.Name = "itemHastCheck";
+            this.itemHastCheck.Size = new System.Drawing.Size(54, 17);
+            this.itemHastCheck.TabIndex = 5;
+            this.itemHastCheck.Text = "Haste";
+            this.itemHastCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemMastCheck
+            // 
+            this.itemMastCheck.AutoSize = true;
+            this.itemMastCheck.Location = new System.Drawing.Point(157, 277);
+            this.itemMastCheck.Name = "itemMastCheck";
+            this.itemMastCheck.Size = new System.Drawing.Size(63, 17);
+            this.itemMastCheck.TabIndex = 6;
+            this.itemMastCheck.Text = "Mastery";
+            this.itemMastCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemStamCheck
+            // 
+            this.itemStamCheck.AutoSize = true;
+            this.itemStamCheck.Location = new System.Drawing.Point(157, 169);
+            this.itemStamCheck.Name = "itemStamCheck";
+            this.itemStamCheck.Size = new System.Drawing.Size(64, 17);
+            this.itemStamCheck.TabIndex = 7;
+            this.itemStamCheck.Text = "Stamina";
+            this.itemStamCheck.UseVisualStyleBackColor = true;
+            // 
+            // itemType
+            // 
+            this.itemType.FormattingEnabled = true;
+            this.itemType.Items.AddRange(new object[] {
+            "Head",
+            "Shoulders",
+            "Chest",
+            "Hands",
+            "Waist",
+            "Legs",
+            "Boots",
+            "Shield",
+            "1 Handed Sword",
+            "2 Handed Sword",
+            "1 Handed Mace",
+            "2 Handed Mace",
+            "Bow",
+            "Rifle"});
+            this.itemType.Location = new System.Drawing.Point(18, 188);
+            this.itemType.Name = "itemType";
+            this.itemType.Size = new System.Drawing.Size(121, 21);
+            this.itemType.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Item Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Item Worth";
+            // 
+            // itemWorth
+            // 
+            this.itemWorth.FormattingEnabled = true;
+            this.itemWorth.Items.AddRange(new object[] {
+            "Poor",
+            "Common",
+            "Uncommon",
+            "Rare",
+            "Epic",
+            "Legendary",
+            "Artifact",
+            "Heirloom"});
+            this.itemWorth.Location = new System.Drawing.Point(18, 238);
+            this.itemWorth.Name = "itemWorth";
+            this.itemWorth.Size = new System.Drawing.Size(121, 21);
+            this.itemWorth.TabIndex = 10;
+            // 
+            // itemInt
+            // 
+            this.itemInt.Location = new System.Drawing.Point(229, 210);
+            this.itemInt.Name = "itemInt";
+            this.itemInt.Size = new System.Drawing.Size(43, 20);
+            this.itemInt.TabIndex = 17;
+            this.itemInt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemInt_KeyPress);
+            // 
+            // itemStr
+            // 
+            this.itemStr.Location = new System.Drawing.Point(229, 188);
+            this.itemStr.Name = "itemStr";
+            this.itemStr.Size = new System.Drawing.Size(43, 20);
+            this.itemStr.TabIndex = 16;
+            this.itemStr.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemStr_KeyPress);
+            // 
+            // itemStam
+            // 
+            this.itemStam.Location = new System.Drawing.Point(229, 166);
+            this.itemStam.Name = "itemStam";
+            this.itemStam.Size = new System.Drawing.Size(43, 20);
+            this.itemStam.TabIndex = 15;
+            this.itemStam.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemStam_KeyPress);
+            // 
+            // itemMas
+            // 
+            this.itemMas.Location = new System.Drawing.Point(229, 276);
+            this.itemMas.Name = "itemMas";
+            this.itemMas.Size = new System.Drawing.Size(43, 20);
+            this.itemMas.TabIndex = 20;
+            this.itemMas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemMas_KeyPress);
+            // 
+            // itemHas
+            // 
+            this.itemHas.Location = new System.Drawing.Point(229, 254);
+            this.itemHas.Name = "itemHas";
+            this.itemHas.Size = new System.Drawing.Size(43, 20);
+            this.itemHas.TabIndex = 19;
+            this.itemHas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemHas_KeyPress);
+            // 
+            // itemAgi
+            // 
+            this.itemAgi.Location = new System.Drawing.Point(229, 232);
+            this.itemAgi.Name = "itemAgi";
+            this.itemAgi.Size = new System.Drawing.Size(43, 20);
+            this.itemAgi.TabIndex = 18;
+            this.itemAgi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.itemAgi_KeyPress);
+            // 
+            // itemDesc
+            // 
+            this.itemDesc.Location = new System.Drawing.Point(18, 130);
+            this.itemDesc.Name = "itemDesc";
+            this.itemDesc.Size = new System.Drawing.Size(254, 20);
+            this.itemDesc.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Item Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Item Name";
+            // 
+            // itemName
+            // 
+            this.itemName.Location = new System.Drawing.Point(18, 88);
+            this.itemName.Name = "itemName";
+            this.itemName.Size = new System.Drawing.Size(254, 20);
+            this.itemName.TabIndex = 23;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 336);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.itemName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.itemDesc);
+            this.Controls.Add(this.itemMas);
+            this.Controls.Add(this.itemHas);
+            this.Controls.Add(this.itemAgi);
+            this.Controls.Add(this.itemInt);
+            this.Controls.Add(this.itemStr);
+            this.Controls.Add(this.itemStam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.itemWorth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.itemType);
+            this.Controls.Add(this.itemStamCheck);
+            this.Controls.Add(this.itemMastCheck);
+            this.Controls.Add(this.itemHastCheck);
+            this.Controls.Add(this.itemAgilCheck);
+            this.Controls.Add(this.itemIntCheck);
+            this.Controls.Add(this.itemStrCheck);
+            this.Controls.Add(this.addItemBtn);
+            this.Controls.Add(this.dbBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dbBtn;
+        private System.Windows.Forms.Button addItemBtn;
+        private System.Windows.Forms.CheckBox itemStrCheck;
+        private System.Windows.Forms.CheckBox itemIntCheck;
+        private System.Windows.Forms.CheckBox itemAgilCheck;
+        private System.Windows.Forms.CheckBox itemHastCheck;
+        private System.Windows.Forms.CheckBox itemMastCheck;
+        private System.Windows.Forms.CheckBox itemStamCheck;
+        private System.Windows.Forms.ComboBox itemType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox itemWorth;
+        private System.Windows.Forms.TextBox itemInt;
+        private System.Windows.Forms.TextBox itemStr;
+        private System.Windows.Forms.TextBox itemStam;
+        private System.Windows.Forms.TextBox itemMas;
+        private System.Windows.Forms.TextBox itemHas;
+        private System.Windows.Forms.TextBox itemAgi;
+        private System.Windows.Forms.TextBox itemDesc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox itemName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        //Item item = new Item();
+        //ItemDB iDB;
+        Form form2;
+        int stam, str, intl, agi, has, mas = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //form2 = new Form2();
+            //iDB = new ItemDB();
+        }
+
+        private void addItemBtn_Click(object sender, EventArgs e)
+        {
+            int itemID = 0;
+            if (isTextBoxNull(itemName) == 0 || isTextBoxNull(itemDesc) == 0)
+            {
+                MessageBox.Show("MissingArgumentsError: You haven't filled in all the options");
+            }
+            else
+            {
+                Boolean[] checkedStats = new bool[] { itemStamCheck.Checked, itemStrCheck.Checked, itemIntCheck.Checked, itemAgilCheck.Checked, itemHastCheck.Checked, itemMastCheck.Checked };
+                String[] typeNames = new string[] { itemStamCheck.Text, itemStrCheck.Text, itemIntCheck.Text, itemAgilCheck.Text, itemHastCheck.Text, itemMastCheck.Text };
+
+                for (int i = 0; i < checkedStats.Length; i++)
+                {
+                    validateChecked(typeNames[i], checkedStats[i]);
+                }
+                //item.addItem(itemID += 1, itemName.Text, itemDesc.Text, itemType.Text, itemWorth.Text, stam, str, intl, agi, mas, has);
+                clearAll();
+            }
+        }
+
+        private void itemType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public String getItemType(int itemID)
+        {
+            return itemType.GetItemText(itemID).ToString();
+        }
+
+        private void itemStam_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void itemStr_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void itemInt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void itemAgi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ItemDatabase_Load(object sender, EventArgs e)
+        {
+            form2.Visible = false;
+        }
+
+        private void itemDBBtn_Click(object sender, EventArgs e)
+        {
+            form2.Visible = true;
+        }
+
+        private void itemMas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void itemHas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        private int isTextBoxNull(TextBox tb)
+        {
+            int isNull;
+            if (tb.Text == "")
+            {
+                isNull = 0;
+            }
+            else
+            {
+                isNull = tb.Text.Length;
+            }
+            return isNull;
+        }
+
+        private void validateChecked(String typeName, Boolean check)
+        {
+            if (check && typeName == "Stamina")
+            {
+                stam = Convert.ToInt32(itemStam.Text);
+            }
+            if (check && typeName == "Strength")
+            {
+                str = Convert.ToInt32(itemStr.Text);
+            }
+            if (check && typeName == "Intellect")
+            {
+                intl = Convert.ToInt32(itemInt.Text);
+            }
+            if (check && typeName == "Agility")
+            {
+                agi = Convert.ToInt32(itemAgi.Text);
+            }
+            if (check && typeName == "Haste")
+            {
+                has = Convert.ToInt32(itemHas.Text);
+            }
+            if (check && typeName == "Mastery")
+            {
+                mas = Convert.ToInt32(itemMas.Text);
+            }
+        }
+        private void clearAll()
+        {
+            stam = 0;
+            str = 0;
+            intl = 0;
+            agi = 0;
+            has = 0;
+            mas = 0;
+            itemName.Clear();
+            itemDesc.Clear();
+            itemStam.Clear();
+            itemStr.Clear();
+            itemInt.Clear();
+            itemAgi.Clear();
+            itemHas.Clear();
+            itemMas.Clear();
+            itemStamCheck.Checked = false;
+            itemStrCheck.Checked = false;
+            itemIntCheck.Checked = false;
+            itemAgilCheck.Checked = false;
+            itemHastCheck.Checked = false;
+            itemMastCheck.Checked = false;
+            itemWorth.SelectedIndex = -1;
+            itemType.SelectedIndex = -1;
+
+        }
+    }
+
+}
